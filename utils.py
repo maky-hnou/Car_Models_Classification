676bb043989239f330b7b0c531b152e85983e7ad
hani <haniyousfi@gmail.com>
2020-02-16 19:10:18 +0100

    Edit utils.py

--- utils.py
+++ utils.py
@@ -27,11 +27,18 @@ def image_to_npy(filename, path, img_size):
 
     """
     data = []
+
+    list_of_categories = [path_.split('/')[-2]
+                          for path_ in natsorted(glob.glob(path + '/*/'),
+                                                 alg=ns.IGNORECASE)]
+    nbr_of_categories = len(list_of_categories)
+    identity = np.identity(nbr_of_categories, dtype='int')
     for img in tqdm(natsorted(glob.glob(path + '/*/*'), alg=ns.IGNORECASE)):
         image = cv2.imread(img)
         image = cv2.resize(image, img_size)
         label = img.split('/')[-2]
-        label = re.sub(r' |\-', '_', label)
+        label_index = list_of_categories.index(label)
+        label = identity[label_index]
         data.append([np.array(image), label])
     random.shuffle(data)
     np.save('{}_data.npy'.format(filename), data)
